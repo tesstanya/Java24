f86a888a0903e7d087fa8d085358d2321de138b2
tesstanya <tesstanya@gmail.com>
2015-10-19 22:26:37 -0700

    Added Hour 5 Code

---
+++ Hour5/src/Squared.java
@@ -0,0 +1,11 @@
+class Squared {
+	public static void main(String[] args) {
+		int x = 10, y = 20;
+		int result = x * x + y * y;
+		
+		System.out.println("The value of x is: " + x);
+		System.out.println("The value of y is: " + y);
+		System.out.println("The sum of x squared and y squared is: " + result);
+		
+	}
+}
